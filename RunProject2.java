d19da17164f4d558d73302ef0662527858040abd
mbw84 <mbw84@scarletmail.rutgers.edu>
2021-03-02 19:18:49 -0500

    Update RunProject2.java

--- RunProject2.java
+++ RunProject2.java
@@ -6,7 +6,7 @@
 public class RunProject2 {
 	/**
 	 * Calls run() method in Kiosk class.
-	 * @param args an array of command line arguments.
+	 * @param args an array of command line arguments,
 	 */
 	public static void main(String[] args){
         new PayrollProcessing().run();
